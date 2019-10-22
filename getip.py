f4cf8ab4a1ccbdc28e1714952a0d0cd6689b8e1d
reelaiah <reelaiah@163.com>
2019-10-22 17:52:08 +0800

    更换ipip.net网站获取公网ip

--- getip.py
+++ getip.py
@@ -1,13 +1,14 @@
 import requests
 from lxml import etree
-def get_ip(url='http://www.baidu.com/s?wd=ip'):
+def get_ip(url='https://www.ipip.net/ip/'):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" }
     response = requests.get(url,headers=headers)
     response.encoding='utf-8'
     # with open('1.html','wb') as fp:
     #     fp.write(response.text.encode('utf-8'))
     html = etree.HTML(response.text)
-    content =  html.xpath("//div/table//tr/td/span[@class='c-gap-right']/text()")
+    # content =  html.xpath("//div/table//tr/td/span[@class='c-gap-right']/text()")
+    content = html.xpath('//div//tr/td/span/a/text()')
     # print(content)
     try:
         ip = str(content[0]).split(':')[-1]
