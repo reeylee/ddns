ca11e1197c44cb199d2ce5cfc5c727ace675911f
reelaiah <reelaiah@163.com>
2019-10-22 17:34:27 +0800

    百度获取ip测试

--- getip.py
+++ getip.py
@@ -4,10 +4,16 @@ def get_ip(url='http://www.baidu.com/s?wd=ip'):
     headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.120 Safari/537.36" }
     response = requests.get(url,headers=headers)
     response.encoding='utf-8'
+    # with open('1.html','wb') as fp:
+    #     fp.write(response.text.encode('utf-8'))
     html = etree.HTML(response.text)
     content =  html.xpath("//div/table//tr/td/span[@class='c-gap-right']/text()")
-    ip = str(content[0]).split(':')[-1]
-    print(ip)
+    # print(content)
+    try:
+        ip = str(content[0]).split(':')[-1]
+    except:
+        ip = '无法获取ip'
+    return ip
 
 if __name__ == "__main__":
-    get_ip()
+    print(get_ip())
